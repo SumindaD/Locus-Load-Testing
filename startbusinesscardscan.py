51312c6c13b23cdaee42fad6561b9199d7bdde89
Suminda De Silva <sumindaniro@gmail.com>
2019-09-23 10:29:25 +0530

    Implemented startbusinesscardscan endpoint

---
+++ startbusinesscardscan.py
@@ -0,0 +1,20 @@
+from locust import HttpLocust, TaskSet, task
+import json
+
+class WebsiteTasks(TaskSet):
+    @task
+    def send(self):
+        with open('startbusinesscardscan.txt') as json_file:
+            jsonData = json.load(json_file)
+
+            self.client.post(
+                url="/suminda/startbusinesscardscan",
+                data=json.dumps(jsonData),
+                auth=None,
+                headers={"content-type":"application/json", "jwt_token": "test"}
+            )
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 5000
+    max_wait = 15000
